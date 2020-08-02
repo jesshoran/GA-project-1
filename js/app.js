548e633274f11ccc9dc927a4f2e723013a83407d
lakergirl702 <jesshoran87@gmail.com>
2020-08-02 07:52:55 -0700

    working on playGame

--- js/app.js
+++ js/app.js
@@ -1,10 +1,26 @@
 let playerMove = null
 let computerMove = null
 
+const $choice = ('.choice')
+const $rock = $('#rock')
+const $paper = $('#paper')
+const $scissors = $('#scissors')
+const $lizzard = $('#lizzard')
+const $spock = $('#spock')
+
 moves = ["rock", "paper", "scissors", "lizzard", "spock"]
 
 const randomIndex = Math.floor(Math.random()*5)
 computerMove = moves[randomIndex]
+
+playGame = () => {
+
+}
+
+
+
+$choice.on("click", playGame)
+
 
 
 
