d30549c3cef0ecdabb19c79cac12d68e9840c4a5
lakergirl702 <jesshoran87@gmail.com>
2020-08-02 07:29:45 -0700

    variables defined

---
+++ js/app.js
@@ -0,0 +1,13 @@
+let playerMove = null
+let computerMove = null
+
+moves = ["rock", "paper", "scissors", "lizzard", "spock"]
+
+const randomIndex = Math.floor(Math.random()*5)
+computerMove = moves[randomIndex]
+
+
+
+
+
+
